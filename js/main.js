7f79f283c1e449d2244e415815ccb73a4e8ad7d8
franklin-raph <igboekwulusifranklin@gmail.com>
2023-09-14 17:34:40 +0100

    update

--- js/main.js
+++ js/main.js
@@ -2,13 +2,22 @@
 document.querySelector(".sign-in-options-back-btn").addEventListener("click", ()=>{
     document.querySelector("#form2").style.left = "150%"
     document.querySelector("#form1").style.left = "50%"
-    console.log("Hello");
 })
 
 document.querySelector(".sign-in-options").addEventListener("click", ()=>{
     document.querySelector("#form1").style.left = "-50%"
     document.querySelector("#form2").style.left = "50%"
-    console.log("Hiiii");
 })
 
-console.log(document.querySelector(".sign-in-options-back-btn"));
+document.querySelector(".sign-in-to-org").addEventListener("click", ()=>{
+    document.querySelector("#form2").style.left = "150%"
+    document.querySelector("#form1").style.left = "50%"
+})
+
+document.querySelector(".next-btn").addEventListener("click", ()=> {
+    if(document.querySelector("input").value === ""){
+        console.log(document.querySelector("#form1 .error-text").style)
+        document.querySelector("#form1 .error-text").style.display = "block"
+    }
+})
+
